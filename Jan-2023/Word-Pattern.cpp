7e41b7fbbbf5cfd2c3141a419db005cbadf648f6
Vatsal Bhatt <114211721+VatsBhatt11@users.noreply.github.com>
2023-01-03 14:45:09 +0530

    Create Word-Pattern.cpp

---
+++ Jan-2023/Word-Pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) 
+    {
+        map<char, string>chToWordMap;
+        map<string, char>wordToChMap;
+        s.push_back(' ');
+        string currWord = "";
+        int chIdx = 0;
+        for (int  i = 0; i < s.size(); i++)
+        {
+            if(s[i] == ' ')
+            {
+                char currCh = pattern[chIdx++];
+                if (chToWordMap.count(currCh))
+                {
+                    string alreadyMappedWord = chToWordMap[currCh];
+                    if (alreadyMappedWord != currWord) return false; //one character map's to two words
+                }
+                else if (wordToChMap.count(currWord))
+                {
+                    char alreadyMappedCh = wordToChMap[currWord];
+                    if (alreadyMappedCh != currCh) return false; //one word map's to two characters
+                }
+                else
+                {
+                    chToWordMap[currCh] = currWord;
+                    wordToChMap[currWord] = currCh;
+                }
+                currWord = "";
+            }
+            else currWord.push_back(s[i]);
+        }
+        return (chIdx == pattern.size());
+    }
+};
