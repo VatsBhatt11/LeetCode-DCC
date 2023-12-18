3740f503cfd77744132fd0bf77c0c607e3f52316
Vatsal Bhatt <114211721+VatsBhatt11@users.noreply.github.com>
2023-12-18 11:51:44 +0530

    Commit Maximum Product Difference Between Two Pairs.cpp

---
+++ Dec-23/18th_Dec.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int mn1=INT_MAX,mn2=mn1,mx1=INT_MIN,mx2=mx1;
+        for(auto it:nums)
+        {
+            if(mx1<it)
+            {
+                mx2=mx1;
+                mx1=it;
+            }
+            else if(mx2<it)
+                mx2=it;
+            if(mn1>it)
+            {
+                mn2=mn1;
+                mn1=it;
+            }
+            else if(mn2>it)
+                mn2=it;
+        }
+        return (mx1*mx2) - (mn1*mn2);
+    }
+};
