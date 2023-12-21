bb6b9b3e4f60cebcd477d8b4292ade20e76c1225
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-12-21 19:53:58 +0530

    Commit Widest Vertical Area Between Two Points Containing No Points.cpp

---
+++ Dec-23/21th_Dec.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        vector<int> v;
+        for (auto it : points)
+            v.push_back(it[0]);
+        sort(v.begin(), v.end());
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++)
+            ans = max(ans, v[i + 1] - v[i]);
+        return ans;
+    }
+};
