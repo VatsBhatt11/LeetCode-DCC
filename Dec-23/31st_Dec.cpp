86f5b8c6bd9e42b7e2494812c3b912ff42c3b758
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-12-31 11:08:29 +0530

    Commit Largest Substring Between Two Equal Characters

---
+++ Dec-23/31st_Dec.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        vector<int>v(26,-1);
+        int ans=-1;
+        for(int i=0;i<s.size();i++)
+        {
+            if(v[s[i]-'a']==-1)
+                v[s[i]-'a'] = i;
+            else
+                ans=max(ans,i-v[s[i]-'a']-1);
+        }
+        return ans;
+    }
+};
