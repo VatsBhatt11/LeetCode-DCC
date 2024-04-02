454cbbcfac46c101f178fc56ceb75a9fa030bc86
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2024-04-02 18:24:17 +0530

    Solved Isomorphic String problem.

---
+++ April-2024/2nd April.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        unordered_map<char, char> m1, m2;
+        for (int i = 0; i < n; i++) {
+            if ((m1.count(s[i]) && m1[s[i]] != t[i]) ||
+                (m2.count(t[i]) && m2[t[i]] != s[i]))
+                return false;
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
+        }
+        return true;
+    }
+};
